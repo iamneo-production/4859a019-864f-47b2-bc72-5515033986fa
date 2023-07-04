648142986c0446bb3e483d3e602685c5d78e6e0a
Niyathi512 <gannavarapuniyathi@gmail.com>
2023-07-04 14:34:26 +0000

    template for dotnet and react folders

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,40 +1,24 @@
-import React from "react";
+import logo from './logo.svg';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  } from "react-router-dom";
 
-// import Login from "./Components/Login/Login";
-// import Signup from "./Components/Signup/Signup";
-// import Home from "./Components/HomePage";
-// import Cart from "./Components/Cart";
-// import Order from "./Components/Order";
-import Dashboard from "./Components/Dashboard";
-// import ViewOrders from "./Components/ViewOrders";
-import AddProduct from "./Components/AddProduct";
-// import {Auth,Admin} from "./Components/Auth/Auth";
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          {/* <Route exact path="/" element = {<Login/>}></Route>
-          <Route path="/signup" element = {<Signup/>}></Route>
-          <Route element={<Auth/>}>
-            <Route path="/home" element = { <Home/>  }></Route>
-            <Route path="/cart" element = {<Cart/> }></Route>
-            <Route path="/order" element = { <Order/> }></Route>
-          </Route>
-          <Route element={<Admin/>}> */}
-            <Route path="/admin" element = {<Dashboard/>}></Route>
-            <Route path="/admin/orders" element = {<ViewOrders/>}></Route>
-            <Route path="/addProduct" element = {<AddProduct/>}></Route>
-          {/* </Route>  */}
-        </Routes>
-      </Router>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
