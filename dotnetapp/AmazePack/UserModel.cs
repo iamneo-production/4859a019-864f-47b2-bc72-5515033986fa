22705bbef9ac74fa487351a0e167ed124dcda614
saitej777 <saitejagubbala2016@gmail.com>
2023-07-05 06:35:01 +0000

    changes  in UserModel

--- dotnetapp/AmazePack/UserModel.cs
+++ dotnetapp/AmazePack/UserModel.cs
@@ -10,11 +10,11 @@ namespace AmazePack
     {
         public int id { get; set; }
         public string userId { get; set; }
-        public string email { get; set; }
-        public string password { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
         public string username { get; set; }
         public string mobileNumber { get; set; }
         public Boolean active { get; set; } 
-        public string role { get; set; }
+        public string userRole { get; set; }
     }
 }
